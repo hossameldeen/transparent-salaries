a0d2a0b3aa397c2a65813a850e82d5be53dd4595
Hossam El-Deen <hossameldeenfci@gmail.com>
2018-06-13 17:44:36 +0200

    Added material style. Added an example usage of a material button. Next is deciding on the layout & using it for real isA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material';
 
 import { AppComponent } from './components/app/app.component';
 
@@ -8,7 +10,9 @@ import { AppComponent } from './components/app/app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
