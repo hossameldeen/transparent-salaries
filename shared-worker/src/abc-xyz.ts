008bf7097c1d3e48a49ea0112e15e3d4faeee62e
Hossam El-Deen <hossameldeenfci@gmail.com>
2018-07-24 11:48:54 +0200

    Example usage of abc.xyz

--- shared-worker/src/abc-xyz.ts
+++ shared-worker/src/abc-xyz.ts
@@ -1,7 +1,23 @@
 /*
  * This file is used as entrypoint for a shared-worker. However, seems like angular expects it to be a module. So, I'm
  * adding a dummy export statement in the end.
+ * 
+ * NOTE: DO NOT NAME THIS FILE `shared-worker.ts`. An error is thrown during build if you do so.
+ * 
+ * Note: You can't use code from `app` project, which actually makes sense, or there would be a circular dependency.
+ * Something I haven't checked though is whether `app` can use code from this project.
  */
+
+(<any>self).onconnect = (connectEvent: MessageEvent) => {
+  const messagePort: MessagePort = (connectEvent.ports as MessagePort[])[0]
+
+  messagePort.postMessage("You're connected to worker!")
+  console.log('hi')
+
+  const y = new DatArchive('deet')
+  const z = new SharedWorker('abc')
+}
+
 
 /**
  * A
