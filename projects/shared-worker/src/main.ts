3caf3e8d0c72e3b3841bf29be20eb058aa575695
Hossam El-Deen <hossameldeenfci@gmail.com>
2018-07-29 06:33:50 +0200

    Added shared-worker example usage

--- projects/shared-worker/src/main.ts
+++ projects/shared-worker/src/main.ts
@@ -1,12 +1,16 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// TODO: Remove this unnecessary import. Check: https://github.com/Microsoft/TypeScript/issues/14877
+import { } from '@angular/core';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+// Note: If you have no `import`s, you'll need to wrap this in an immediately-invoked function or something.
+// Check: https://github.com/Microsoft/TypeScript/issues/14877
+// I don't want to wrap in a function, in order to easily remove the webpack lines from the artifacts
+declare const self: SharedWorker.SharedWorkerGlobalScope;
 
-if (environment.production) {
-  enableProdMode();
+self.onconnect = (connectEvent: MessageEvent) => {
+  // TODO: I have no idea why it's an array
+  const messagePort: MessagePort = connectEvent.ports[0]
+  // TODO: Not necessary good to JSON.stringify stuff without thinking.
+  messagePort.onmessage = (msgEvt: MessageEvent) => messagePort.postMessage(`Worker received ${JSON.stringify(msgEvt.data)}`)
+  // Note: onmessage has started the port. Otherwise, you'd have needed to call `port.start()`
+  messagePort.postMessage("You're connected to worker!")
 }
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
--- src/main.ts
+++ src/main.ts
@@ -11,3 +11,10 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+const sharedWorker = new SharedWorker('assets/shared-worker/main.js')
+sharedWorker.port.onmessage = (msgEvt: MessageEvent) => console.log('Received from worker:', msgEvt.data)
+
+const packets = ['Hi', {abc: 'def'}]
+for (const p of packets)
+  sharedWorker.port.postMessage(p) // Note: onmessage has started the port. Otherwise, you'd have needed to call `port.start()`
