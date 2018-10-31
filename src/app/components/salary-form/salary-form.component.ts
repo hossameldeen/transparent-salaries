da866dd5500c2bb9434a829782636c943ceccc78
Hossam El-Deen <hossameldeenfci@gmail.com>
2018-10-31 14:19:17 +0200

    Updated SalaryForm: Added tracking changes.

--- src/app/components/salary-form/salary-form.component.ts
+++ src/app/components/salary-form/salary-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UtilService } from 'src/app/services/util.service';
+import { Salary } from 'src/app/models/salary.model';
 
 @Component({
   selector: 'app-salary-form',
@@ -8,9 +9,18 @@ import { UtilService } from 'src/app/services/util.service';
 })
 export class SalaryFormComponent implements OnInit {
 
-  constructor(readonly utilService: UtilService) { }
+  //         <input matInput placeholder="{{cc.placeholder}}" type="{{cc.inputType}}" [value]="salary(row.currentData)[cc.propertyName]" (input)="salary(row.currentData)[cc.propertyName]=$event.target.value" [disabled]="row.currentData.locked">
+  readonly salary: Salary;
+
+  constructor(utilService: UtilService) {
+    this.salary = new Salary(utilService.getCurrentMonth(), "", "", "", "", "")
+  }
 
   ngOnInit() {
+  }
+
+  onInput($event: Event, fieldPropertyName: string) {
+    this.salary[fieldPropertyName] = ($event.target as HTMLInputElement).value
   }
 
 }
