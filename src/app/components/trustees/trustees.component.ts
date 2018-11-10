5751d72f47faf0315ef4238ff359ffe8d8396fa8
Hossam El-Deen <hossameldeenfci@gmail.com>
2018-11-10 03:40:05 +0200

    Updated TrusteeComponent: Added Remove/Untrust button.

--- src/app/components/trustees/trustees.component.ts
+++ src/app/components/trustees/trustees.component.ts
@@ -5,6 +5,7 @@ import { ProgressBarService } from 'src/app/services/progress-bar.service';
 import { MatSnackBar } from '@angular/material';
 import { Profile, encode as encodeProfile, decode as decodeProfile } from 'src/app/models/profile.model';
 import { UtilService } from 'src/app/services/util.service';
+import { DBRow } from 'src/app/models/db-row.model';
 
 @Component({
   selector: 'app-trustees',
@@ -14,8 +15,9 @@ import { UtilService } from 'src/app/services/util.service';
 export class TrusteesComponent implements OnInit {
 
   @Input() profileDatArchive: DatArchive;
+  @Input() isOwner: boolean;
 
-  trustees: Array<{ datUrl: string, displayNameState: { kind: "loading" } | { kind: "loaded", displayName: string } | { kind: "errored", err: any }}>;
+  trustees: Array<TrusteeEntry>;
 
   constructor(
     readonly utilService: UtilService,
@@ -40,14 +42,11 @@ export class TrusteesComponent implements OnInit {
       let atLeastOneFailed = false
       for (const ret of trusteesOrFailures) {
         if (ret.status === "succeeded") {
-          const trusteeUrl = ret.row.dbRowData.datUrl
-          const trusteeObject:
-            { datUrl: string, displayNameState: { kind: "loading" } | { kind: "loaded", displayName: string } | { kind: "errored", err: any }} =
-            { datUrl: trusteeUrl, displayNameState: { kind: "loading"} } // making it like that to use closures with mutability. Yuck!
+          const trusteeObject: TrusteeEntry = { dbRow: ret.row, removing: false, displayNameState: { kind: "loading"} } // making it like that to use closures with mutability. Yuck!
 
           this.trustees.push(trusteeObject)
           this.progressBarService.pushLoading()
-          DatArchive.load(trusteeUrl)
+          DatArchive.load(ret.row.dbRowData.datUrl)
             .then(trusteeArchive => this.dbService.readRow<Profile>(trusteeArchive, 'profiles', this.dbService.PROFILE_ROW_UUID, decodeProfile))
             .then(profileRow => {
               trusteeObject.displayNameState = { kind: 'loaded', displayName: profileRow.dbRowData.displayName }
@@ -73,4 +72,34 @@ export class TrusteesComponent implements OnInit {
     }
   }
 
+  /**
+   * The index is just for optimization. In most cases, I shouldn't need to loop over the array.
+   */
+  async removeTrustee(trusteeEntry: TrusteeEntry, trusteeEntryIndex: number) {
+    try {
+      this.progressBarService.pushLoading()
+      trusteeEntry.removing = true
+
+      await this.dbService.deleteRow<Trustee>(this.profileDatArchive, 'trustees', trusteeEntry.dbRow.uuid)
+
+      if (trusteeEntryIndex < this.trustees.length && this.trustees[trusteeEntryIndex].dbRow.uuid !== trusteeEntry.dbRow.uuid)
+        trusteeEntryIndex = this.trustees.findIndex(tdbr => tdbr.dbRow.uuid === trusteeEntry.dbRow.uuid)
+
+      // TODO: Add a check that trusteeEntryIndex !== -1
+      this.trustees.splice(trusteeEntryIndex, 1)
+    }
+    catch (e) {
+      this.snackBar.open("Couldn't untrust the trustee for some reason :(", "Dismiss")
+      trusteeEntry.removing = false
+    }
+    finally {
+      this.progressBarService.popLoading()
+    }
+  }
+}
+
+type TrusteeEntry = {
+  dbRow: DBRow<Trustee>,
+  removing: boolean,
+  displayNameState: { kind: "loading" } | { kind: "loaded", displayName: string } | { kind: "errored", err: any }
 }
