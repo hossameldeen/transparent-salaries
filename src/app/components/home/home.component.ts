d30659791abbb83daef7d4e4afa1046d020e08ee
Hossam El-Deen <hossameldeenfci@gmail.com>
2018-11-15 09:20:29 +0200

    Update HomeComponent: Navigated to selected/created profile on select/createProfile.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { ProfileService, ProfileStateKind } from 'src/app/services/profile.service';
+import {Component} from '@angular/core';
+import {ProfileService, ProfileStateKind} from 'src/app/services/profile.service';
+import {Router} from '@angular/router';
+import {UtilService} from 'src/app/services/util.service';
 
 @Component({
   selector: 'app-home',
@@ -9,6 +11,32 @@ export class HomeComponent {
 
   ProfileStateKind = ProfileStateKind;
 
-  constructor(readonly profileService: ProfileService) { }
+  constructor(
+    readonly profileService: ProfileService,
+    private readonly router: Router
+  ) { }
+
+  async createProfile() {
+    await this.profileService.createProfile()
+    await this.navigatedIfSelected()
+  }
+
+  async selectProfile() {
+    await this.profileService.selectProfile()
+    await this.navigatedIfSelected()
+  }
+
+  private async navigatedIfSelected() {
+    switch (this.profileService.stateSubject.value.kind) {
+      case ProfileStateKind.ProfileSelected:
+        await this.router.navigateByUrl(`/profile/${this.profileService.stateSubject.value.datArchive.url.slice(6)}`)
+        break;
+      case ProfileStateKind.ProfileNotSelected:
+        // do nothing
+        break;
+      default:
+        UtilService.assertNever(this.profileService.stateSubject.value)
+    }
+  }
 }
 
